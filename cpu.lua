fec8a523d481b3e568221dca9347c91cd215c9cb
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-17 04:23:11 -0300

    minor fixes

--- cpu.lua
+++ cpu.lua
@@ -4,15 +4,16 @@ local util = require "util"
 local memory = {}
 local pointer = {}
 
---[[
+love.graphics.setPointSize(10)
+
 local f = 0
-for y = 0, 360, 1 do
-	for x = 0, 640, 1 do
+for y = 0, 360/love.graphics.getPointSize(), 1 do
+	for x = 0, 640/love.graphics.getPointSize(), 1 do
 		f = f + 1
-		memory[f] = {x,y,util.math.regrad3(1,640,x),util.math.regrad3(1,360,y),255,255}
+		memory[f] = {x*love.graphics.getPointSize(),y*love.graphics.getPointSize(),util.math.regrad3(1,640/love.graphics.getPointSize(),x),util.math.regrad3(1,360/love.graphics.getPointSize(),y),1,1}
+		pointer[x .. 'x' .. y] = #memory + 1
 	end
 end
-]]
 
 local instruct =
 {
@@ -22,13 +23,12 @@ local instruct =
 			memory[p] = {x,y,r,g,b,a}
 		else
 			pointer[x .. 'x' .. y] = #memory + 1
-			table.insert(memory,{x,y,r,g,b,a})
+			table.insert(memory,{x*love.graphics.getPointSize(),y*love.graphics.getPointSize(),r,g,b,a})
 		end
 	end,
 	MOVE = function(x,y,vx,vy)
 		local p1,p2 = memory[pointer[x .. 'x' .. y]],memory[pointer[vx .. 'x' .. vy]]
 		memory[p1],memory[p2] = memory[p2],memory[p1]
-		memory[x][y],memory[vx][vy] = memory[vx][vy],memory[x][y]
 	end
 }
 
--- main.lua
+++ main.lua
@@ -7,13 +7,11 @@ end
 function love.keypressed(key)
 end
 
-local teste = 1
-
 function love.update()
 end
 
 function love.draw()
-	machine.instruct.SET(util.random(0,love.graphics.getWidth()),util.random(0,love.graphics.getHeight()),util.random(0,1),util.random(0,1),util.random(0,1),util.random(0,1))
+	machine.instruct.SET(util.random(0,love.graphics.getWidth()/love.graphics.getPointSize()),util.random(0,love.graphics.getHeight()/love.graphics.getPointSize()),util.random(0,1),util.random(0,1),util.random(0,1),util.random(0,1))
 	love.graphics.points(machine.memory)
 	love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS()), 1, 1)
 end
